c87d13c2ece06b6a3fccb61a5d67208cb625728a
Субботин Никита <subbotin@vbankcenter.ru>
2021-08-24 17:28:08 +0300

    Копирование

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const massive = ['Первый','Второй', 'Третий', 'Четвертый']
+// const massive = ['Первый','Второй', 'Третий', 'Четвертый']
 
-console.log(massive);
+// console.log(massive);
 
- const person = new Object  ()
-    person.name= 'Nikita' 
-    person.subname = 'Subbotin'
-    person.age = 23
+//  const person = new Object  ()
+//     person.name= 'Nikita' 
+//     person.subname = 'Subbotin'
+//     person.age = 23
 
- console.log(person)
+//  console.log(person)
 
-function sum(a , b) {
-  return a+b/b
+// function sum(a , b) {
+//   return a+b/b
+// }
+//   const result = sum (1, 5)
+
+// console.log(result)
+
+let button1 = document.getElementById('button1')
+
+function click () {
+  alert ('Yay')
 }
-  const result = sum (1, 5)
+button1.onclick = click
 
-alert(result)
-console.log(result)
+let btn2 = document.getElementById('btn2')
+
+function myFunction() {
+  let copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("copy");
+  alert("Скопированный текст: " + copyText.value);
+}
