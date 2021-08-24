782eb059cc27e4f999928cf56b26560d7c960eef
Субботин Никита <subbotin@vbankcenter.ru>
2021-08-24 16:05:01 +0300

    Js

---
+++ index.js
@@ -0,0 +1,18 @@
+const massive = ['Первый','Второй', 'Третий', 'Четвертый']
+
+console.log(massive);
+
+ const person = new Object  ()
+    person.name= 'Nikita' 
+    person.subname = 'Subbotin'
+    person.age = 23
+
+ console.log(person)
+
+function sum(a , b) {
+  return a+b/b
+}
+  const result = sum (1, 5)
+
+alert(result)
+console.log(result)
