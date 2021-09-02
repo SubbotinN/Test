167d3512752d1814af1ba327323413e3fbbc5ea7
Субботин Никита <subbotin@vbankcenter.ru>
2021-09-02 17:37:20 +0300

    Test

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// const massive = ['Первый','Второй', 'Третий', 'Четвертый']
-
 // console.log(massive);
 
 //  const person = new Object  ()
@@ -15,19 +13,58 @@
 //   const result = sum (1, 5)
 
 // console.log(result)
+// let button1 = document.getElementById("button1");
 
-let button1 = document.getElementById("button1");
+// function click() {
+//   alert("Yay");
+// }
+// button1.onclick = click;
 
-function click() {
-  alert("Yay");
-}
-button1.onclick = click;
+// let btn2 = document.getElementById("btn2");
 
-let btn2 = document.getElementById("btn2");
+// function myFunction() {
+//   let copyText = document.getElementById("myInput");
+//   copyText.select();
+//   document.execCommand("copy");
+//   alert("Скопированный текст: " + copyText.value);
+// }
 
-function myFunction() {
-  let copyText = document.getElementById("myInput");
-  copyText.select();
-  document.execCommand("copy");
-  alert("Скопированный текст: " + copyText.value);
-}
+// alert("Привет,Мир!");
+
+// let message = "Hello";
+// alert(message);
+
+// let user = 'Nikita' , age = 23 , message = 'Hello'
+
+// let admin = "Джон";
+
+//  let name = admin;
+
+//  alert(name);
+
+// let namePlanet
+// let userName
+
+// let name = "Nikita";
+// alert(`Привет ${name}!`);
+
+// let age = prompt("Сколько тебе лет?");
+
+// alert(`Тебе ${age} лет!`);S
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert(isBoss);
+
+// let userName = prompt("Как тебя зовут?");
+// alert(`Тебя зовут ${userName}`);
+
+// let value = true;
+// //  alert(typeof value);
+
+// value = String(value);
+// alert(typeof value);
+123;
+31231;
+323123;
+31231;
